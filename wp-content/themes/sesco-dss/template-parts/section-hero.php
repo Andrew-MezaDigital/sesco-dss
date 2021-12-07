4007f3a660c372e52bb905285f6abb3a49d8f76a
Meza <andrew@mezadigital.io>
2021-12-07 12:47:48 -0500

    Create hero template part

---
+++ wp-content/themes/sesco-dss/template-parts/section-hero.php
@@ -0,0 +1,7 @@
+<section class="pageHero" style="background-image:url('<?php echo 'the image'; ?>');">
+    <div class="grid">
+        <div class="gridCell">
+            <h1 class="page-title screen-reader-text"><?php the_title(); ?></h1>
+        </div>
+    </div>
+</section>
