8dfc3ade0592d9d15d4cc41042beef09e43c4cc2
Meza <andrew@mezadigital.io>
2021-12-07 12:47:16 -0500

    Create homepage template

---
+++ wp-content/themes/sesco-dss/front-page.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The homepage template
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+        <?php get_template_part( 'template-parts/section', 'hero') ?>
+
+		<?php
+		if ( have_posts() ) :
+
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+
+				/*
+				 * Include the Post-Type-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/content', get_post_type() );
+
+			endwhile;
+
+		else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif;
+		?>
+
+	</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
