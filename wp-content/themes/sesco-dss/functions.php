279f4fbd8ae79076d2fc9d606748944dd37b010b
Meza <andrew@mezadigital.io>
2021-12-22 17:39:53 -0500

    Setup splide js plugin

--- wp-content/themes/sesco-dss/functions.php
+++ wp-content/themes/sesco-dss/functions.php
@@ -144,6 +144,7 @@ function sesco_dss_scripts() {
 	wp_style_add_data( 'sesco-dss-style', 'rtl', 'replace' );
 
 	wp_enqueue_script( 'sesco-dss-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'splide', get_template_directory_uri() . '/js/splide/splide.min.js', array(), null, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
