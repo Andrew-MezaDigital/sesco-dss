0f0d90b88d519d5bee73acce9ac95df4022d73e5
Meza <andrew@mezadigital.io>
2021-12-24 10:19:13 -0500

    Style and adjust homepage services section

--- wp-content/themes/sesco-dss/template-parts/section-categories.php
+++ wp-content/themes/sesco-dss/template-parts/section-categories.php
@@ -6,7 +6,7 @@
     'hide_empty' => false,
 ) ); ?>
 <?php if (!empty($term_query) && !is_wp_error($term_query)) : ?>
-  <section id="services">
+  <section id="services" class="no-pt">
     <h2 class="screen-reader-text">Services</h2>
     <ul class="grid up-3">
       <?php foreach ($term_query->terms as $term) : ?>
@@ -15,7 +15,9 @@
         <?php $image_id = get_term_meta($term->term_id, 'category_image', true ); ?>
         <?php $image = wp_get_attachment_image($image_id, 'large'); ?>
         <li class="cell">
-          <?php echo $image ? $image : ''; ?>
+          <div class="img-w">
+            <a href="<?php echo $term_link; ?>"><?php echo $image ? $image : ''; ?></a>
+          </div>
           <h3><a href="<?php echo $term_link; ?>"><?php echo $term->name; ?></a></h3>
           <?php echo $excerpt ? '<p>' . $excerpt . '</p>' : ''; ?>
         </li>
