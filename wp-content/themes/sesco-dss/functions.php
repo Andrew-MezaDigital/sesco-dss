ebae61cb4ee13d8c410262d009878e92c765297f
Meza <andrew@mezadigital.io>
2022-01-17 20:42:16 -0500

    install mixitup

--- wp-content/themes/sesco-dss/functions.php
+++ wp-content/themes/sesco-dss/functions.php
@@ -142,6 +142,7 @@ add_action( 'widgets_init', 'sesco_dss_widgets_init' );
 function sesco_dss_scripts() {
 	wp_enqueue_style( 'sesco-dss-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'sesco-dss-style', 'rtl', 'replace' );
+	wp_enqueue_script( 'mixitup', get_template_directory_uri() . '/js/mixitup.min.js', array(), null, true );
 	wp_enqueue_script( 'splide', get_template_directory_uri() . '/js/splide.min.js', array(), null, true );
 	wp_enqueue_script( 'sesco-dss-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 	wp_enqueue_script( 'scripts', get_template_directory_uri() . '/js/scripts.js', 'splide', null, true );
