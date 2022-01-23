17e340d7e2e2ccd07864964367004f3f9ef4a384
Meza <andrew@mezadigital.io>
2022-01-23 16:35:13 -0500

    Setup store section

--- wp-content/themes/sesco-dss/template-parts/content-page.php
+++ wp-content/themes/sesco-dss/template-parts/content-page.php
@@ -25,14 +25,39 @@
 		<div class="row">
 			<h2 class="screen-reader-text">Our Stores</h2>
 			<?php while ($stores->have_posts()) : $stores->the_post(); ?>
-				<div class="cell fill">
+				<?php $store_photo = get_field('store_photo'); ?>
+				<div class="cell lg-50">
 					<h3><?php the_title(); ?></h3>
-					<ul class="store-info">
-						<li><?php the_field('store_phone'); ?></li>
-						<li><?php the_field('store_email'); ?></li>
-						<li>
-					</ul>
-					<iframe width="600" height="450" style="border:0" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=<?php echo urlencode(get_field('store_address')); ?>&key=AIzaSyDx4NdBfDH3awgzkVFLMEAPbE5Gge3IgtU"></iframe>
+					<iframe width="600" height="250" style="border:0" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=<?php echo urlencode(get_field('store_address')); ?>&key=AIzaSyDx4NdBfDH3awgzkVFLMEAPbE5Gge3IgtU"></iframe>
+					<?php if (has_post_thumbnail()) : ?>
+						<h4>Store Front</h4>
+						<div class="img-w slim">
+							<?php the_post_thumbnail(); ?>
+						</div>
+					<?php endif; ?>
+					<div class="row">
+						<div class="cell fill">
+							<h4>Phone</h4>
+							<p class="line-break"><a href="tel:<?php the_field('store_phone'); ?>" title="Call the <?php echo get_the_title(); ?>"><span class="fas fa-phone-alt"></span>Call us</a></p>
+							<h4>Email</h4>
+							<p class="line-break"><a href="mailto:<?php the_field('store_email'); ?>" title="Email the <?php echo get_the_title(); ?>"><span class="fas fa-envelope"></span>Email us</a></p>
+						</div>
+						<div class="cell fill">
+							<?php if (have_rows('store_hours')) : ?>
+								<h4>Business Hours</h4>
+								<table class="store-hours">
+									<tbody>
+										<?php while (have_rows('store_hours')) : the_row(); ?>
+											<tr>
+												<th><?php the_sub_field('store_hours_day'); ?></th>
+												<td><?php echo get_sub_field('store_hours_open') . " - " . get_sub_field('store_hours_close'); ?></td>
+											</tr>
+										<?php endwhile; ?>
+									</tbody>
+								</table>
+							<?php endif; ?>
+						</div>
+					</div>
 				</div>
 			<?php endwhile; wp_reset_postdata(); ?>
 		</div>
