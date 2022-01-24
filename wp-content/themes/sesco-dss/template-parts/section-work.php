ae3c3a4c9ecf76f0db0d877b54891b8d67a601c4
Meza <andrew@mezadigital.io>
2022-01-24 15:15:20 -0500

    Setup page overrides for work section

--- wp-content/themes/sesco-dss/template-parts/section-work.php
+++ wp-content/themes/sesco-dss/template-parts/section-work.php
@@ -7,9 +7,8 @@
     'posts_per_page' => 4
   ); 
   $work = new WP_Query($args);
-  $work_url = site_url() . get_field('section_work_url');
-  $work_cta = get_field('section_work_cta');
-  $work_title = get_field('section_work_header');
+  $link = get_field('section_work_link');
+  $title = get_field('section_work_headline');
   $edit_homepage_link = get_edit_post_link(get_the_ID());
   ?>
 
@@ -18,23 +17,30 @@
       <div class="row mb ha-between va-center">
         <div class="cell auto">
           <h2>
-            <a href="<?php echo $work_url . '#latest-work'; ?>"><?php echo $work_title ? $work_title : 'Our Latest Work Samples'; ?></a>
-            <?php echo is_user_logged_in() ? '<a href="' . $edit_homepage_link . '" class="post-edit-link">Edit this content</a>' : ''; ?>
+            <?php if ($link) : ?>
+              <a href="<?php echo $link['url']; ?>" target="<?php echo $link['target'] ? esc_attr($link['target']) : '_self'; ?>"><?php echo $title ? $title : 'Our Latest Work Samples'; ?></a>
+            <?php else : ?>
+              <?php echo $title ? $title : 'Our Latest Work Samples'; ?>
+            <?php endif; ?>
+            <?php echo is_user_logged_in() ? '<a href="' . $edit_homepage_link . '" class="post-edit-link">Edit this</a>' : ''; ?>
           </h2>
         </div>
-        <div class="cell auto">
-          <a href="<?php echo $work_url . '#latest-work'; ?>" class="btn secondary"><?php echo $work_cta ? $work_cta : 'More of our work'; ?>&nbsp;&raquo;</a>
-          <?php echo is_user_logged_in() ? '<a href="' . $edit_homepage_link . '" class="post-edit-link">Edit this content</a>' : ''; ?>
-        </div>
+        <?php if ($link) : ?>
+          <div class="cell auto">
+            <a href="<?php echo $link['url']; ?>" target="<?php echo $link['target'] ? esc_attr($link['target']) : '_self'; ?>" class="btn secondary"><?php echo $link['title'] ? $link['title'] : 'View more'; ?>&nbsp;&raquo;</a>
+            <?php echo is_user_logged_in() ? '<a href="' . $edit_homepage_link . '" class="post-edit-link">Edit this</a>' : ''; ?>
+          </div>
+        <?php endif; ?>
       </div>
       <ul class="list-work grid up-4">
         <?php while ($work->have_posts()) : $work->the_post(); ?>
           <li id="work-<?php echo get_the_ID(); ?>" class="cell">
-            <?php $work_item_url = $work_url . '#work-' . get_the_ID(); ?>
-            <a href="<?php echo $work_item_url; ?>" class="img-w"><?php the_post_thumbnail(); ?></a>
+            <div class="img-w">
+              <?php the_post_thumbnail(); ?>
+            </div>
             <h3 class="line-break">
-              <a href="<?php echo $work_item_url; ?>"><?php the_title(); ?></a>
-              <?php echo is_user_logged_in() ? edit_post_link('Edit this content') : ''; ?>
+              <?php the_title(); ?>
+              <?php echo is_user_logged_in() ? edit_post_link() : ''; ?>
             </h3>
             <?php the_excerpt(); ?>
           </li>
@@ -52,8 +58,10 @@
   ); 
   $work = new WP_Query($args);
   $services = new WP_Query(array('post_type' => 'service'));
-  $work_title = get_field('section_work_header');
-  $edit_page_link = get_edit_post_link(get_the_ID());
+  $term = get_queried_object();
+  $title = get_field('section_work_headline', $term);
+  $term_id = $term->term_id;
+  $term_tax = get_term($term_id)->taxonomy;
   ?>
 
   <?php if ($work->have_posts()) : ?>
@@ -61,8 +69,8 @@
       <div class="row mb ha-between va-center">
         <div class="cell auto">
           <h2>
-            <?php echo $work_title ? $work_title : 'Our Latest Work Samples'; ?>
-            <?php echo is_user_logged_in() ? '<a href="' . $edit_page_link . '" class="post-edit-link">Edit this content</a>' : ''; ?>
+            <?php echo $title ? $title : 'Our Latest Work Samples'; ?>
+            <?php echo is_user_logged_in() ? '<a href="' . get_edit_term_link($term_id, $term_tax) . '" class="post-edit-link">Edit this</a>' : ''; ?>
           </h2>
         </div>
         <?php if ($services->have_posts()) : ?>
@@ -87,7 +95,6 @@
             <?php endforeach; ?>
           <?php endif; ?>
           <li id="work-<?php echo get_the_ID(); ?>" class="mix<?php echo $service_classes; ?> cell">
-            <?php $work_item_url = $work_url . '#work-' . get_the_ID(); ?>
             <a href="<?php echo get_the_post_thumbnail_url(); ?>" class="glightbox img-w" data-glightbox="<?php echo 'title: ' . get_the_title() . '; description: .custom-desc-' . get_the_ID() ?>">
               <?php // Setup srcset ?>
               <?php the_post_thumbnail('medium'); ?>
@@ -105,7 +112,7 @@
             </div>
             <h3 class="line-break">
               <?php the_title(); ?>
-              <?php echo is_user_logged_in() ? edit_post_link('Edit this content') : ''; ?>
+              <?php echo is_user_logged_in() ? edit_post_link() : ''; ?>
             </h3>
           </li>
           <?php $service_classes = ''; ?>
