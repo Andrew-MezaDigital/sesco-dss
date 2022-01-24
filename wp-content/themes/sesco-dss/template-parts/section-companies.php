f510edfcc971e706f28b5a6ceb9229c6006f6b5f
Meza <andrew@mezadigital.io>
2022-01-24 14:24:26 -0500

    Setup logo carousel on taxonomy pages

--- wp-content/themes/sesco-dss/template-parts/section-companies.php
+++ wp-content/themes/sesco-dss/template-parts/section-companies.php
@@ -8,20 +8,26 @@
 
     <section class="companies">
       <div class="row ha-center">
-        <div class="cell auto ta-center">
-          <h2>
+        <div class="cell lg-100 ta-center">
+          <h2 class="ba-center">
             <?php the_field('section_companies_headline', $term); ?>
             <?php echo is_user_logged_in() ? '<a href="' . get_edit_term_link($term_id, $term_tax) . '" class="post-edit-link">Edit this</a>' : ''; ?>
           </h2>
-          <?php echo get_field('section_companies_subheadline') ? '<p class="subhead">' . get_field('section_companies_subheadline') . '</p>' : ''; ?>
-          <?php if (have_rows($companies)) : ?>
+          <?php echo get_field('section_companies_subheadline', $term) ? '<p class="subhead">' . get_field('section_companies_subheadline', $term) . '</p>' : ''; ?>
+          <?php if (have_rows('section_companies_companies', $term)) : ?>
             <div class="logos">
               <div class="splide__track">
                 <ul class="splide__list">
-                  <?php while (have_rows($companies)) : the_row(); ?>
-                    <li class="splide__slide">
-                      <?php echo get_sub_field('company_name'); ?>
-                    </li>
+                  <?php while (have_rows('section_companies_companies', $term)) : the_row(); ?>
+                    <?php $logo = get_sub_field('company_logo', $term); ?>
+                    <?php if (!empty($logo)) : ?>
+                      <li class="splide__slide">
+                        <div class="img-w">
+                          <img src="<?php echo esc_url($logo['sizes']['medium']); ?>" alt="<?php echo esc_attr($logo['alt']); ?>" class="logo" />
+                        </div>
+                        <h3 class="screen-reader-text"><?php echo get_sub_field('company_name', $term); ?></h3>
+                      </li>
+                    <?php endif; ?>
                   <?php endwhile; ?>
                 </ul>
               </div>
