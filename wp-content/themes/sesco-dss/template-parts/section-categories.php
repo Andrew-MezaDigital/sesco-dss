57335fc91828f9d8bd6d82b4b97708e33169e58d
Meza <andrew@mezadigital.io>
2022-01-25 18:31:10 -0500

    Fix post edit links for categories section

--- wp-content/themes/sesco-dss/template-parts/section-categories.php
+++ wp-content/themes/sesco-dss/template-parts/section-categories.php
@@ -10,10 +10,14 @@
     <h2 class="screen-reader-text">Services</h2>
     <ul class="cards grid up-3">
       <?php foreach ($term_query->terms as $term) : ?>
-        <?php $term_link = get_term_link($term); ?>
-        <?php $excerpt = get_term_meta($term->term_id, 'category_excerpt', true ); ?>
-        <?php $image_id = get_term_meta($term->term_id, 'category_image', true ); ?>
-        <?php $image = wp_get_attachment_image($image_id, 'medium'); ?>
+        <?php
+          $term_id = $term->term_id;
+          $term_tax = get_term($term_id)->taxonomy;
+          $term_link = get_term_link($term);
+          $excerpt = get_term_meta($term_id, 'category_excerpt', true );
+          $image_id = get_term_meta($term_id, 'category_image', true );
+          $image = wp_get_attachment_image($image_id, 'medium');
+        ?>
         <li class="cell">
           <div class="card card-lg">
             <a href="<?php echo $term_link; ?>" class="img-w">
@@ -22,7 +26,7 @@
             <div class="card-copy">
               <h3 class="card-title">
                 <a href="<?php echo $term_link; ?>"><?php echo $term->name; ?></a>
-                <?php echo is_user_logged_in() ? edit_post_link() : ''; ?>
+                <?php echo is_user_logged_in() ? '<a href="' . get_edit_term_link($term_id, $term_tax) . '" class="post-edit-link">Edit this</a>' : ''; ?>
               </h3>
               <?php echo $excerpt ? '<p>' . $excerpt . '</p>' : ''; ?>
             </div>
