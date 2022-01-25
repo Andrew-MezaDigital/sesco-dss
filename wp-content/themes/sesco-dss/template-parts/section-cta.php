1e3af57f863c9c4ffb11a95dd3ffba3ffdc9d4ac
Meza <andrew@mezadigital.io>
2022-01-25 18:25:36 -0500

    Update cta section styles

--- wp-content/themes/sesco-dss/template-parts/section-cta.php
+++ wp-content/themes/sesco-dss/template-parts/section-cta.php
@@ -6,15 +6,15 @@
     <?php $term_id = $term->term_id; ?>
     <?php $term_tax = get_term($term_id)->taxonomy; ?>
 
-    <section class="page-cta bg-primary">
+    <section class="page-cta">
       <div class="row ha-center">
         <div class="cell auto ta-center">
-          <h2 class="ba-center">
+          <h2 class="alt ba-center">
             <?php the_field('section_cta_headline', $term); ?>
             <?php echo is_user_logged_in() ? '<a href="' . get_edit_term_link($term_id, $term_tax) . '" class="post-edit-link">Edit this</a>' : ''; ?>
           </h2>
           <?php if ($link): ?>
-            <a href="<?php echo esc_url($link['url']); ?>" target="<?php echo $link['target'] ? esc_attr($link['target']) : '_self'; ?>" class="btn secondary ba-center"><?php echo esc_html($link['title']); ?></a>
+            <a href="<?php echo esc_url($link['url']); ?>" target="<?php echo $link['target'] ? esc_attr($link['target']) : '_self'; ?>" class="btn primary ba-center"><?php echo esc_html($link['title']); ?></a>
           <?php endif; ?>
         </div>
       </div>
@@ -27,15 +27,15 @@
   <?php if (get_field('section_cta_show')) : ?>
     <?php $link = get_field('section_cta_link');?>
 
-    <section class="page-cta bg-primary">
+    <section class="page-cta">
       <div class="row ha-center">
         <div class="cell auto ta-center">
-          <h2 class="ba-center">
+          <h2 class="alt ba-center">
             <?php the_field('section_cta_headline'); ?>
             <?php echo is_user_logged_in() ? edit_post_link() : ''; ?>
           </h2>
           <?php if ($link): ?>
-            <a href="<?php echo esc_url($link['url']); ?>" target="<?php echo $link['target'] ? esc_attr($link['target']) : '_self'; ?>" class="btn secondary ba-center"><?php echo esc_html($link['title']); ?></a>
+            <a href="<?php echo esc_url($link['url']); ?>" target="<?php echo $link['target'] ? esc_attr($link['target']) : '_self'; ?>" class="btn primary ba-center"><?php echo esc_html($link['title']); ?></a>
           <?php endif; ?>
         </div>
       </div>
